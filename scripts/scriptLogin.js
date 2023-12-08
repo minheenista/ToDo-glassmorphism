8bcd91869b9199e95b6e7c87ead967c4e848dc46
Mich <anamichelle1308@gmail.com>
2023-12-08 14:21:00 -0600

    :100: All functions

--- scripts/scriptLogin.js
+++ scripts/scriptLogin.js
@@ -1,35 +1,56 @@
 function login() {
-    // valores del form
-    const username = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    return new Promise((resolve, reject) => {
+        // valores del form
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
 
-    // api
-    const api = '';
+        // api con esquema http://
+        const api = 'http://10.1.142.212:3000/users/login';
 
-    // Realiza una solicitud POST a la API
-    fetch(api, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: username,
-            password: password
+        // Realiza una solicitud POST a la API
+        fetch(api, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
         })
-    })
-        .then(response => response.json())
-        .then(data => {
-            // Maneja la respuesta de la API
-            console.log(data);
-            alert('Inicio de sesión exitoso');
-            window.location.href = "../todo/index.html";
+            .then(response => response.json())
+            .then(data => {
+                // Maneja la respuesta de la API
+                console.log(data);
 
-            // Aquí puedes realizar acciones adicionales, como redireccionar a otra página
-        })
-        .catch(error => {
-            // Maneja errores de la solicitud
-            alert('Error al iniciar sesión');
-            window.location.href = "../todo/index.html";
-            console.error('Error:', error);
-        });
+                // Realiza acciones adicionales con la respuesta de la API
+                alert('Inicio de sesión exitoso');
+                window.location.href = "../todo/index.html";
+
+                // Resuelve la promesa con la respuesta de la API
+                resolve(data);
+            })
+            .catch(error => {
+                // Maneja errores de la solicitud
+                alert('Error al iniciar sesión');
+                console.error('Error:', error);
+
+                // Rechaza la promesa con el error
+                reject(error);
+            });
+    });
 }
+
+/* 
+// Ejemplo de cómo puedes manejar la respuesta de la función
+login().then(response => {
+    console.log('Respuesta de la función:', response);
+    // Realiza acciones adicionales con la respuesta de la API
+    alert('Inicio de sesión exitoso');
+    window.location.href = "../todo/index.html";
+})
+    .catch(error => {
+        console.error('Error al llamar a la función:', error);
+        printError(error);
+    });
+ */
--- scripts/scriptRegister.js
+++ scripts/scriptRegister.js
@@ -7,34 +7,56 @@ function register() {
     const password = document.getElementById('password').value;
 
     // api
-    const api = '';
+    const api = 'http://10.1.142.212:3000/users';
 
-    // Realiza una solicitud POST a la API
-    fetch(api, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: name,
-            lastname: lastname,
-            email: email,
-            username: username,
-            password: password
+
+    fetch('http://10.1.142.212:3000/users')
+        .then(res => {
+            alert(res)
+            return res.json();
         })
-    })
-        .then(response => response.json())
         .then(data => {
-            // Maneja la respuesta de la API
+            alert(data)
             console.log(data);
-            alert('Registro exitoso');
-            window.location.href = "../login.html";
-
-            // Aquí puedes realizar acciones adicionales, como redireccionar a otra página
         })
-        .catch(error => {
-            // Maneja errores de la solicitud
-            alert('Error al registrar');
-            console.error('Error:', error);
-        });
+        .catch(error => console.log(error));
 }
+
+// Realiza una solicitud POST a la API
+/* fetch(api, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: name,
+        lastname: lastname,
+        email: email,
+        username: username,
+        svpassword: password
+    })
+})
+    .then(response => {
+        // Verifica si la respuesta fue exitosa (código de estado en el rango 200)
+        if (!response.ok) {
+            // Si la respuesta no fue exitosa, lanza un error para ser manejado en el bloque catch
+            throw new Error('Error en la solicitud');
+        }
+        // Si la respuesta fue exitosa, retorna la respuesta como JSON
+        return response.json();
+    })
+    .then(data => {
+        // Maneja la respuesta de la API
+        console.log(data);
+        alert('Registro exitoso');
+        window.location.href = "../login.html";
+        // Aquí puedes realizar acciones adicionales, como redireccionar a otra página
+    })
+    .catch(error => {
+        // Maneja errores de la solicitud
+        alert('Error al registrar');
+        console.error('Error:', error);
+    }); */
+
+
+
